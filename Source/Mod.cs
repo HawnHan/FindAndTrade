c5ef57603761ddc963e49503a3fc209da8dfecd8
Lana Avery <memekoten@gmail.com>
2026-01-14 21:15:59 +1100

    Init

---
+++ Source/Mod.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace RimMod
+{
+    public class Mod : Verse.Mod
+    {
+        public Mod(ModContentPack content) : base(content)
+        {
+
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            base.DoSettingsWindowContents(inRect);
+        }
+
+        public override string SettingsCategory() => "RimMod.Settings".Translate();
+    }
+}
